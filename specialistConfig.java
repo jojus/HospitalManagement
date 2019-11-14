5694556424a49e8a21d22653555a5a908cd2316b
jojus <25826009+jojus@users.noreply.github.com>
2019-11-14 10:13:06 +0530

    read property file

--- specialistConfig.java
+++ specialistConfig.java
@@ -1,5 +1,39 @@
 package com.rdp.hosptialfrontdesk.constants;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
+
+@Configuration
+@PropertySource("classpath:specialist.properties")
 public class specialistConfig {
+
+	@Value("#{'${specialist.names}'.split(',')}")
+	private List<String> specialistName;
+
+	@Value("#{'${specialist.availableday}'.split(',')}")
+	private List<String> specialistAvailableDay;
+
+	@Value("#{'${specialist.availabletime}'.split(',')}")
+	private List<String> specialistAvailableTime;
+
+	public static PropertySourcesPlaceholderConfigurer propertyScan() {
+		return new PropertySourcesPlaceholderConfigurer();
+	}
+
+	public List<String> getSpecialistName() {
+		return specialistName;
+	}
+
+	public List<String> getSpecialistAvailableDay() {
+		return specialistAvailableDay;
+	}
+
+	public List<String> getSpecialistAvailableTime() {
+		return specialistAvailableTime;
+	}
 
 }
